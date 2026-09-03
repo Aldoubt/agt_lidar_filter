3751d23df5f4079b64293898c25ebd52634372b2
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-03 13:54:05 +0800

    feat: add cropbox filter implementation path

--- src/filters/cropbox_filter.cpp
+++ src/filters/cropbox_filter.cpp
@@ -1,5 +1,9 @@
 #include "agt_lidar_filter/filters/cropbox_filter.hpp"
 
+#include <pcl/filters/crop_box.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+
 namespace agt_lidar_filter
 {
 
@@ -12,9 +16,8 @@ std::string CropBoxFilter::name() const
   return "cropbox";
 }
 
-bool CropBoxFilter::configure(const std::string & namespace_name)
+bool CropBoxFilter::configure(const std::string &)
 {
-  (void)namespace_name;
   return true;
 }
 
@@ -22,7 +25,20 @@ bool CropBoxFilter::process(
   const sensor_msgs::msg::PointCloud2 & input,
   sensor_msgs::msg::PointCloud2 & output)
 {
-  output = input;
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::PointCloud<pcl::PointXYZ> filtered;
+
+  pcl::fromROSMsg(input, cloud);
+
+  pcl::CropBox<pcl::PointXYZ> crop;
+  crop.setInputCloud(cloud.makeShared());
+  crop.setMin(Eigen::Vector4f(min_x_, min_y_, min_z_, 1.0f));
+  crop.setMax(Eigen::Vector4f(max_x_, max_y_, max_z_, 1.0f));
+  crop.filter(filtered);
+
+  pcl::toROSMsg(filtered, output);
+  output.header = input.header;
+
   return true;
 }
 
