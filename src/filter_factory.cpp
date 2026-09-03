4f7e7ef7acb594dec1e0bc26ca3291f6b4820b06
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-03 13:54:14 +0800

    feat: register cropbox and angular filters

--- src/filter_factory.cpp
+++ src/filter_factory.cpp
@@ -1,5 +1,7 @@
 #include "agt_lidar_filter/filter_factory.hpp"
 #include "agt_lidar_filter/filters/blind_radius_filter.hpp"
+#include "agt_lidar_filter/filters/cropbox_filter.hpp"
+#include "agt_lidar_filter/filters/angular_mask_filter.hpp"
 
 namespace agt_lidar_filter
 {
@@ -8,6 +10,14 @@ std::shared_ptr<PointCloudFilter> FilterFactory::create(const std::string & type
 {
   if (type == "blind_radius") {
     return std::make_shared<BlindRadiusFilter>();
+  }
+
+  if (type == "cropbox") {
+    return std::make_shared<CropBoxFilter>();
+  }
+
+  if (type == "angular_mask") {
+    return std::make_shared<AngularMaskFilter>();
   }
 
   return nullptr;
