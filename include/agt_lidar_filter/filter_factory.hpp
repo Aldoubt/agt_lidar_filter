4bd93f9c31d70a3101e1b1aa9cfd9831cd002f44
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-03 13:38:16 +0800

    docs: refine filter factory contract

--- include/agt_lidar_filter/filter_factory.hpp
+++ include/agt_lidar_filter/filter_factory.hpp
@@ -2,15 +2,26 @@
 
 #include <memory>
 #include <string>
+
 #include "agt_lidar_filter/filter_base.hpp"
 
 namespace agt_lidar_filter
 {
 
+/**
+ * Runtime factory for filter plugins.
+ *
+ * The factory isolates node/pipeline logic from concrete filters.
+ * Future implementations may create filters from yaml names such as:
+ * - blind_radius
+ * - cropbox
+ * - angular_mask
+ * - voxel
+ */
 class FilterFactory
 {
 public:
   static std::shared_ptr<PointCloudFilter> create(const std::string & type);
 };
 
-}
+}  // namespace agt_lidar_filter
